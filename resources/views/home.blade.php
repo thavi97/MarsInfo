488a9a0ef95e4ab0f34a9074263b3398b1b78990
Thavi <thavit97@gmail.com>
2020-05-08 23:23:21 +0100

    show temperature

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,19 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
     </head>
     <body>
-      @foreach($url_json as $sol)
-      {{$sol['AT']['av']+32}}<br>
-      <br><br>
-      @endforeach
+      <div class="container">
+        @foreach($url_json as $sol)
+        <div>
+          <p>Average Temperature on Mars: {{$sol['AT']['av']+32}} C</p>
+          <p>Min Temperature on Mars: {{$sol['AT']['mn']+32}} C</p>
+          <p>Max Temperature on Mars: {{$sol['AT']['mx']+32}} C</p>
+        </div>
+        <br>
+        @endforeach
+      </div>
     </body>
 </html>
