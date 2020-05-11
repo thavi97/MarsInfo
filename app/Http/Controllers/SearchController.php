1bc18a4851368aa78aef75f774a091058ab66c5b
Thavi <thavit97@gmail.com>
2020-05-11 16:03:38 +0100

    weather and wind both styled

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,11 +8,15 @@ class SearchController extends Controller
 {
   public function index()
   {
-    $weather_data = json_decode(file_get_contents('https://api.nasa.gov/insight_weather/?api_key=54SAzZS6Ayg2q3gAZmTIFMQRIYkBF8fVkKs2xDOH&feedtype=json&ver=1.0'), true);
-    array_pop($weather_data);
-    array_pop($weather_data);
+    $weather_data = $this->getWeatherData();
     $todays_weather = array_pop($weather_data);
-    return view('home')->with('todays_weather', $todays_weather);
+    $latest_date = $this->getLatestDate($todays_weather);
+    //dd($todays_weather);
+    $data = [
+      'todays_weather' => $todays_weather,
+      'latest_date' => $latest_date
+    ];
+    return view('home')->with('data', $data);
   }
   /**
   * Show the form for creating a new resource.
@@ -21,7 +25,7 @@ class SearchController extends Controller
   */
   public function create()
   {
-    
+
   }
   /**
   * Store a newly created resource in storage.
@@ -32,5 +36,17 @@ class SearchController extends Controller
   public function store(Request $request)
   {
 
+  }
+
+  protected function getWeatherData(){
+    $weather_data = json_decode(file_get_contents('https://api.nasa.gov/insight_weather/?api_key=54SAzZS6Ayg2q3gAZmTIFMQRIYkBF8fVkKs2xDOH&feedtype=json&ver=1.0'), true);
+    array_pop($weather_data);
+    array_pop($weather_data);
+    return $weather_data;
+  }
+
+  protected function getLatestDate($todays_weather){
+    $latest_date = date_format(date_create($todays_weather["First_UTC"]),"F d");
+    return $latest_date;
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,19 @@
 @extends('templates.app')
 @section('weather')
 <div class="row">
-  <div class="col-lg-4">
-    <p class="weather-text">Average Temperature: {{$todays_weather['AT']['av']+32}} C</p>
-  </div>
-  <div class="col-lg-4">
-    <p class="weather-text">Min Temperature: {{$todays_weather['AT']['mn']+32}} C</p>
-  </div>
-  <div class="col-lg-4">
-    <p class="weather-text">Max Temperature: {{$todays_weather['AT']['mx']+32}} C</p>
+  <div class="weather-info-container col-lg-12">
+    <div class="temperature-text-container">
+      <span class="weather-text">
+        {{$data['latest_date']}}<br>
+        <b>{{round($data['todays_weather']['AT']['av']+32, 1)}}°C</b>
+      </span>
+    </div>
+    <div class="wind-speed-container">
+      <span class="weather-text">
+        Wind Speed<br>
+        <b>{{$data['todays_weather']['HWS']['av']}}m/s</b>
+      </span>
+    </div>
   </div>
 </div>
 @endsection
